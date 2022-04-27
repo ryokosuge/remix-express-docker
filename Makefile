041fda1b7083868ddfefcb70bea9018b712958af
ryokosuge <ryo.kosuge@gmail.com>
2022-04-27 23:00:51 +0900

    chore:	nginx

---
+++ Makefile
@@ -0,0 +1,5 @@
+up:
+	$(MAKE) -f docker.mk up
+
+restart:
+	$(MAKE) -f docker.mk up
---
+++ docker.mk
@@ -0,0 +1,8 @@
+build:
+	docker compose build
+
+up:
+	docker compose up --build -d
+
+restart:
+	docker compose restart
